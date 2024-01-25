80b32eececc1431e45054c9cffef6ed88ef1a366
kzb0125 <kzb0125@auburn.edu>
2024-01-25 15:16:34 -0800

    added image assets

--- Clock_App--M4-/ViewController.swift
+++ Clock_App--M4-/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var liveClock: UILabel!
+    // current time
+    let currTime = Date()
+    
+    let dateFormatter = DateFormatter()
+
+    let timeFormat = Date.FormatStyle()
+        .weekday(.abbreviated)
+        .day(.twoDigits)
+        .month(.abbreviated)
+        .year(.defaultDigits)
+        .hour(.twoDigits(amPM: .omitted))
+        .minute(.twoDigits)
+        .second(.twoDigits)
+    
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //liveClock update
+        updateTimestamp()
     }
+    
+    func updateTimestamp() {
+        dateFormatter.dateFormat = "EE, dd MM YYYY hh:mm:ss"
+        let dateString  = dateFormatter.string(from: Date())
+        
+        liveClock.text = "\(dateString)"
+        //liveClock.text = "\(Date().formatted(timeFormat))"
+    }
+    
 
-
+    
+    
+    
 }
 
