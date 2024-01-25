217b0912b4f2c3f327ee666de68d4fad279dfa69
kzb0125 <kzb0125@auburn.edu>
2024-01-25 00:22:09 -0800

    Initial Commit

---
+++ Clock_App--M4-/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clock_App--M4-
+//
+//  Created by Ellie Kim on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
