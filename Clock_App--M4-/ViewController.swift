880c5dd53272363f446ab9acf6dbf597fff768dc
kzb0125 <kzb0125@auburn.edu>
2024-01-25 17:07:46 -0800

    revised dateFormatter

--- Clock_App--M4-/ViewController.swift
+++ Clock_App--M4-/ViewController.swift
@@ -8,40 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var liveClock: UILabel!
-    // current time
-    let currTime = Date()
     
-    let dateFormatter = DateFormatter()
-
-    let timeFormat = Date.FormatStyle()
-        .weekday(.abbreviated)
-        .day(.twoDigits)
-        .month(.abbreviated)
-        .year(.defaultDigits)
-        .hour(.twoDigits(amPM: .omitted))
-        .minute(.twoDigits)
-        .second(.twoDigits)
+    var dateFormatter : DateFormatter {
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "EE, dd MMM YYYY hh:mm:ss"
+        return dateFormat
+    }
     
     var timer = Timer()
+    
+    @IBOutlet weak var liveClock: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //liveClock update
-        updateTimestamp()
+        updateLiveClock()
     }
     
     func updateTimestamp() {
-        dateFormatter.dateFormat = "EE, dd MM YYYY hh:mm:ss"
         let dateString  = dateFormatter.string(from: Date())
         
         liveClock.text = "\(dateString)"
-        //liveClock.text = "\(Date().formatted(timeFormat))"
+        
     }
     
-
+    func updateLiveClock() {
+        let clockTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+        
+    }
+    
+    
     
     
     
